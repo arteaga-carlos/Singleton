9952a72ab466178a66fb1e2b11dddce8ac78a708
arteaga-carlos <59030600+arteaga-carlos@users.noreply.github.com>
2022-05-26 15:49:59 -0400

    Add files via upload

---
+++ Singleton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+
+        public static T _instance;
+
+
+        public static T Instance {
+
+            get {
+
+                if (_instance == null) {
+
+                    GameObject obj = new GameObject();
+
+                    _instance = obj.AddComponent<T>();
+
+                    obj.name = typeof(T).ToString();
+                }
+
+                // returns a gameobject containing a singleton type script attached to it
+                return _instance;
+            }
+        }
+    }
